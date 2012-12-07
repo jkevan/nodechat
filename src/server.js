f5b738e06207b5b93b63a991ef5764788c574da2
Kevan Jahanshahi <kevan.jahanshahi@gmail.com>
2012-12-07 17:45:20 +0100

    add socket.io and serve html pages

--- src/server.js
+++ src/server.js
@@ -1,18 +1,43 @@
 var flatiron = require('flatiron'),
-	app = flatiron.app;
+	app = flatiron.app,
+	fs = require('fs');
 
 app.use(flatiron.plugins.http, {
 	// HTTP options
 });
 
 app.router.get('/', function () {
-	this.res.writeHead(200, { 'Content-Type': 'text/plain' })
-	this.res.end('flatiron ' + flatiron.version);
+	var self = this;
+	fs.readFile('html/home.html',
+		function (err, data) {
+			if (err) {
+				self.res.writeHead(500, { 'Content-Type': 'text/plain' });
+				return self.res.end('Error loading home.html');
+			}
+
+			self.res.writeHead(200);
+			self.res.end(data);
+		});
 });
 
 app.router.get('/talk/:id', function (id) {
-	this.res.writeHead(200, { 'Content-Type': 'text/plain' })
-	this.res.end('talk: ' + id);
+	var self = this;
+	fs.readFile('html/talk.html',
+		function (err, data) {
+			if (err) {
+				self.res.writeHead(500, { 'Content-Type': 'text/plain' });
+				return self.res.end('Error loading talk.html');
+			}
+
+			self.res.writeHead(200);
+			self.res.end(data);
+		});
 });
 
 app.start(8080);
+io = require('socket.io').listen(app.server);
+
+io.sockets.on('connection', function (socket) {
+	console.log("test");
+});
+
