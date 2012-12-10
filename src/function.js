f91ce8a278daf9967c3a6fd32079cb6dfd446adc
maxime- <corinne32@yopmail.com>
2012-12-10 22:19:19 +0100

    Fix connexion nickname, timeout, creation multiroom avec genaration lien quand action quit

---
+++ src/function.js
@@ -0,0 +1,25 @@
+String.prototype.replaceAll = function(stringToFind,stringToReplace)
+{
+	var temp = this;
+	var index = temp.indexOf(stringToFind);
+	while(index != -1)
+	{
+		temp = temp.replace(stringToFind,stringToReplace);
+		index = temp.indexOf(stringToFind);
+	}
+	return temp;
+}
+
+findTextUri = function(messages){
+	var tabMessages = messages;
+	for(var i in tabMessages)
+	{
+		if((tabMessages[i].msg.split(" ").length - 1) >= 5)
+		{
+			var uri_ = tabMessages[i].msg + '_' + tabMessages[i].nickname;
+			uri_ = uri_.replaceAll(" ","-")
+			return	encodeURIComponent(uri_);
+		}
+	}
+	return "";
+}
