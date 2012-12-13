3868519148f25fe5a82b3c200050a7951b06f700
maxime- <corinne32@yopmail.com>
2012-12-13 01:05:17 +0100

    Refactoring, meilleure gestion join quit room

--- src/function.js
+++ src/function.js
@@ -40,26 +40,70 @@ String.prototype.startsWith = function(prefix) {
 	return this.indexOf(prefix) === 0;
 }
 
-function joinRoom(nickname, room, socket){
-	if(room)
+function joinRoom(nickname, channel, socket){
+	if(channel)
 	{
-		if(!users[room])
-			users[room] = new Array();
-		users[room][nickname] = nickname;
-		socket.set("room", room);
-		socket.join(room);
-		io.sockets.in(room).emit('update_console', 'SERVER', nickname + ' est maintenant connecté');
-		console.log(nickname + ' a rejoint la room ' + room);
+		if(!users[channel])
+			users[channel] = new Array();
+		users[channel][nickname] = nickname;
+		socket.set("room", channel);
+		socket.join(channel);
+		io.sockets.in(channel).emit('update_console', 'SERVER', nickname + ' est maintenant connecté');
+		socket.emit('update_console', 'SERVER', 'vous êtes connecté sur ' + channel);
+		socket.emit('update_users', users[channel]);
+		console.log(nickname + ' a rejoint la room ' + channel);
+		
+		db.collection('talk', function (err, collection) {
+            collection.findOne({room:channel}, function(err, document){ // Voir pour changer ID
+                if(document != null){
+                    var oldMessages = document.messages;
+                    for(var i in oldMessages){
+                        socket.emit("update_console", oldMessages[i].nickname, oldMessages[i].msg);
+                    }
+                }
+            });
+        });
+		console.log(users[channel]);
 		// Rediriger vers la conversation de la room
 	}
 	else
 	{
-		// Envoyer message erreur
+		socket.emit("update_console", 'SERVER', 'le nom de room n\'est pas valide');
+		console.log('le nom de room n\'est pas valide');
+	}
+}
+
+function quitRoom(nickname, channel, socket){
+	if(channel)
+	{
+		if(users[channel][nickname])
+		{
+			socket.leave(channel);
+			delete users[room][nickname];
+			io.sockets.in(channel).emit('update_console', 'SERVER', nickname + ' a quitté la room');
+			io.sockets.in(channel).emit('update_users', users[channel]);
+			socket.emit('update_console', 'SERVER', 'vous êtes déconnecté de ' + channel);
+			console.log(nickname + ' a quitté la room ' + channel);
+			// Redirection vers une autre room (/quit) ou reste sur le même si /quit anotherroom
+			// var rooms = io.sockets.manager.roomClients[socket.id]; // Permet d'avoir la liste des rooms du mec
+			// Do not forget socket.set("room", otherchannel);
+		}
+		else
+		{
+			socket.emit("update_console", 'SERVER', 'Vous ne faite pas partie de cette room');
+			console.log(nickname + ' ne fais pas partie de la room ' + channel);
+		}
+	}
+	else
+	{
+		socket.emit("update_console", 'SERVER', 'le nom de room n\'est pas valide');
 		console.log('le nom de room n\'est pas valide');
 	}
 }
 
 function saveMsg(nickname, msg, channel){
+	io.sockets.in(channel).emit('update_console', nickname, msg);
+	console.log(channel + ' ' + nickname + ' a envoyé :' + msg);
     db.collection('talk', function (err, collection) {
         collection.findOne({room: channel}, function(err, document){
             if(document == null){
@@ -71,6 +115,27 @@ function saveMsg(nickname, msg, channel){
                 {nickname:nickname, msg:msg}}}, {safe: true},  function(err, doc){
 					console.log('UP '+ channel + ' ' + nickname + 'a envoyé ' + msg);
                 });
+            }
+        });
+    });
+}
+
+function closeTalk(channel){
+    db.collection('talk', function (err, collection) {
+        collection.findOne({room: channel}, function(err, document){
+            if(document != null){
+				var uri_ = findTextUri(document.messages);
+				if(uri_)
+				{
+					collection.update({room: channel}, {closed: true, uri:uri_}, {safe: true},  function(err, doc){
+						console.log('Fermeture de la room '+ channel);
+						console.log('l uri de ' + channel + ' est ' + uri_);
+					});
+					io.sockets.in(channel).emit("update_console", "SERVER", "La room " + channel + " est close. Retrouvez l'historique de la discussion à l'adresse suivante : <a href=\"/room/" + uri_ + "\">/room/" + uri_+ "</a>");
+				}
+				else{
+					console.log('Impossible de générer l uri, aucunes phrases suppérieur à 5 mots.');
+				}
             }
         });
     });
--- src/server.js
+++ src/server.js
@@ -29,26 +29,6 @@ app.router.get('/', function () {
 		});
 });
 
-function closeTalk(channel){
-    db.collection('talk', function (err, collection) {
-        collection.findOne({room: channel}, function(err, document){
-            if(document != null){
-				var uri_ = findTextUri(document.messages);
-				if(uri_)
-				{
-					collection.update({room: channel}, {closed: true, uri:uri_}, {safe: true},  function(err, doc){
-						console.log('Fermeture de la room '+ channel);
-						console.log('l uri de ' + channel + ' est ' + uri_);
-					});
-					io.sockets.in(channel).emit("update_console", "SERVER", "La room " + channel + " est close. Retrouvez l'historique de la discussion à l'adresse suivante : <a href=\"/room/" + uri_ + "\">/room/" + uri_+ "</a>");
-				}
-				else
-					console.log('Impossible de générer l uri, aucunes phrases suppérieur à 5 mots.');
-            }
-        });
-    });
-}
-
 app.start(8080);
 io = require('socket.io').listen(app.server);
 
@@ -57,20 +37,6 @@ io.sockets.on('connection', function (socket) {
     socket.on('add_user', function(nickname, channel){
         socket.set("nickname", nickname);
 		joinRoom(nickname, channel, socket);
-        db.collection('talk', function (err, collection) {
-            collection.findOne({room:channel}, function(err, document){ // Voir pour changer ID
-                if(document != null){
-                    var oldMessages = document.messages;
-                    for(var i in oldMessages){
-                        socket.emit("update_console", oldMessages[i].nickname, oldMessages[i].msg);
-                    }
-					console.log('add_user ' + channel);
-					//sockets.emit("update_console", "SERVER", "vous êtes connecté");
-					io.sockets.in(channel).emit("update_console", "SERVER", nickname + " est connecté");
-                    //io.sockets.emit('update_users', io.sockets.clients(channel));
-                }
-            });
-        });
     });
 
     socket.on('send_msg', function(data, room){
@@ -82,81 +48,54 @@ io.sockets.on('connection', function (socket) {
 				}
 				else if(data.startsWith("/quit"))
 				{
-					socket.get('room', function(err, room){
-						console.log('P'+room+"P");
-					});
-					var ch = data.split(" ", 2);
-					if(ch[1])
-					{
-						
-						console.log(nickname + ' a quitter la room ' + ch[1]);
-						var rooms = io.sockets.manager.roomClients[socket.id];
-						console.log('before');
-						for(var i in rooms){
-							console.log(''+i);
-						}
-						socket.leave(ch[1]);
-						console.log('after');
-						for(var i in rooms){
-							console.log(''+i);
-						}
-					}
-					else
-					{
-						console.log(nickname + ' a quitter la room ' + room);
-						var rooms = io.sockets.manager.roomClients[socket.id];
-						console.log('before :' + rooms +':');
-						for(var i in rooms){
-							console.log(':'+i+':');
-						}
-						socket.leave(room);
-						console.log('after :' + rooms +':');
-						for(var i in rooms){
-							console.log(':'+i+':');
-						}
-						//socket.leave(room);
-						//console.log(nickname + ' a quitter la room ' + room);
-					}
+					quitRoom(nickname, data.split(" ", 2), socket);			
 				}
 				else
 				{
 					saveMsg(nickname, data, room);
-					io.sockets.in(room).emit('update_console', nickname, data);
-					console.log('send msg' + room);
 				}
-					//closeTalk(room);
-            }else{
+            }
+			else{
                 console.log(err);
-				console.log('fail');
             }
         });
     });
 
-   app.router.get('/room/:uri_', function (uri_) {
-	var self = this;
-	fs.readFile('html/talk.html',
-		function (err, data) {
-			if (err) {
-				self.res.writeHead(500, { 'Content-Type': 'text/plain' });
-				return self.res.end('Error loading talk.html');
+	app.router.get('/room/:uri_', function (uri_) {
+		var self = this;
+		fs.readFile('html/talk.html',
+			function (err, data) {
+				if (err) {
+					self.res.writeHead(500, { 'Content-Type': 'text/plain' });
+					return self.res.end('Error loading talk.html');
+				}
+
+				self.res.writeHead(200);
+				self.res.end(data);
+			});
+	});
+
+	socket.on('disconnect', function(){
+		socket.get('room', function(err, room){
+			if(!err){
+				socket.get('nickname', function(err, nickname){
+					if(!err){
+						if(users[room][nickname])
+						{
+							delete users[room][nickname]; // Refaire en supprimant le mec de toutes les rooms
+						}
+						socket.broadcast.emit('update_console', 'SERVER', nickname + ' s\'est déconnecté');
+					}
+					else{
+						console.log(err);
+					}
+				});
 			}
-
-			self.res.writeHead(200);
-			self.res.end(data);
+			else{
+				console.log(err);
+			}
 		});
+	});
 });
 
- socket.on('disconnect', function(){
-        socket.get('nickname', function(err, nickname){
-            if(!err){
-               // delete users[nickname];
-                io.sockets.emit('update_users', users);
-                socket.broadcast.emit('update_console', 'SERVER', nickname
-                    + ' s\'est déconnecté');
-            }else {
-                console.log(err);
-            }
-        });
-    });
-
-});
+ //closeTalk(room); // Voir Timer pas de message genre un timer avec callback par room
