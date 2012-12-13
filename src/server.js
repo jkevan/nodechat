b7dca0437f63a3a96327ab5212ef1b363375c696
kevan <kevan.jahanshahi@ingesup.com>
2012-12-13 21:47:05 +0100

    add next/prev

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ var db = new Db('nodechat', server);
 var users = new Array();
 var liveTalks = new Array();
 var savedTalks = new Array();
-var lastTalks = new Array();
+var allURIs = new Array();
 
 eval(fs.readFileSync('function.js', encoding="ascii"));
 eval(fs.readFileSync('templates.js', encoding="ascii"));
@@ -48,13 +48,31 @@ app.router.get('/talk/:uri_', function (uri) {
 					if(err){
 						console.log(err)
 					}else{
+                        var uri_next_index = allURIs.indexOf(uri) != -1 ? allURIs.indexOf(uri) + 1 : null;
+                        var uri_prev_index = allURIs.indexOf(uri) != -1 ? allURIs.indexOf(uri) - 1 : null;
 						var htmlMessages = "";
 						for(var i in document.messages){
 							htmlMessages += plates.bind(messageTemplate, document.messages[i]);
-                            console.log(htmlMessages);
 						}
+
+                        var result = plates.bind(talkTemplate, {messages: htmlMessages});
+                        if(allURIs[uri_next_index]){
+                            var nextMap = plates.Map();
+                            nextMap.where('href').is('href').insert("uri");
+                            var nextLink = plates.bind(talkLinkTemplate, {link: "discussion suivante"});
+                            nextLink = plates.bind(nextLink, {uri: "/talk/" + allURIs[uri_next_index]}, nextMap);
+                            result = plates.bind(result, {next: nextLink});
+                        }
+                        if(allURIs[uri_prev_index]){
+                            var prevMap = plates.Map();
+                            prevMap.where('href').is('href').insert("uri");
+                            var prevLink = plates.bind(talkLinkTemplate, {link: "discussion précédente"});
+                            prevLink = plates.bind(prevLink, {uri: "/talk/" + allURIs[uri_prev_index]}, prevMap);
+                            result = plates.bind(result, {prev: prevLink});
+                        }
+
 						self.res.writeHead(200);
-						self.res.end(plates.bind(talkTemplate, {messages: htmlMessages}));
+						self.res.end(result);
 					}
 				});
 			});
@@ -64,56 +82,65 @@ app.router.get('/talk/:uri_', function (uri) {
 app.start(8080);
 io = require('socket.io').listen(app.server);
 
-io.sockets.on('connection', function (socket) {
-    socket.on('add_user', function(nickname, channel){
-        socket.set("nickname", nickname);
-		joinRoom(nickname, channel, socket);
-    });
+db.collection('talk', function (err, collection) {
+    collection.find().toArray(function(err, documents){
+        for (var i in documents){
+            allURIs.push(documents[i].uri);
+        }
 
-    socket.on('send_msg', function(data, room){
-        socket.get('nickname', function(err, nickname){
-            if(!err){
-				if(data.startsWith("/join"))
-				{
-					joinRoom(nickname, data.split(" ", 2)[1], socket);
-				}
-				else if(data.startsWith("/quit"))
-				{
-					quitRoom(nickname, data.split(" ", 2)[1], socket);			
-				}
-				else
-				{
-					saveMsg(nickname, data, room, socket);
-				}
-            }
-			else{
-                console.log(err);
-            }
+        io.sockets.on('connection', function (socket) {
+            socket.on('add_user', function(nickname, channel){
+                socket.set("nickname", nickname);
+                joinRoom(nickname, channel, socket);
+            });
+
+            socket.on('send_msg', function(data, room){
+                socket.get('nickname', function(err, nickname){
+                    if(!err){
+                        if(data.startsWith("/join"))
+                        {
+                            joinRoom(nickname, data.split(" ", 2)[1], socket);
+                        }
+                        else if(data.startsWith("/quit"))
+                        {
+                            quitRoom(nickname, data.split(" ", 2)[1], socket);
+                        }
+                        else
+                        {
+                            saveMsg(nickname, data, room, socket);
+                        }
+                    }
+                    else{
+                        console.log(err);
+                    }
+                });
+            });
+
+            socket.on('disconnect', function(){
+                socket.get('room', function(err, room){
+                    if(!err){
+                        socket.get('nickname', function(err, nickname){
+                            if(!err && nickname != null){
+                                if(users[room][nickname])
+                                {
+                                    delete users[room][nickname]; // Refaire en supprimant le mec de toutes les rooms
+                                }
+                                socket.broadcast.emit('update_console', 'SERVER', nickname + ' s\'est déconnecté');
+                            }
+                            else{
+                                if(err)
+                                    console.log(err);
+                                if(nickname == null)
+                                    console.log("tentative de connection avec un nickname null")
+                            }
+                        });
+                    }
+                    else{
+                        console.log(err);
+                    }
+                });
+            });
         });
     });
-
-	socket.on('disconnect', function(){
-		socket.get('room', function(err, room){
-			if(!err){
-				socket.get('nickname', function(err, nickname){
-					if(!err && nickname != null){
-						if(users[room][nickname])
-						{
-							delete users[room][nickname]; // Refaire en supprimant le mec de toutes les rooms
-						}
-						socket.broadcast.emit('update_console', 'SERVER', nickname + ' s\'est déconnecté');
-					}
-					else{
-						if(err)
-							console.log(err);
-						if(nickname == null)
-							console.log("tentative de connection avec un nickname null")
-					}
-				});
-			}
-			else{
-				console.log(err);
-			}
-		});
-	});
 });
+
--- src/templates.js
+++ src/templates.js
@@ -1,2 +1,3 @@
-var talkTemplate = "<html><head></head><body><div id='messages'></div></body></html>"
-var messageTemplate = "<span><b id='nickname'></b>: <span id='msg'></span></span><br>"
+var talkTemplate = "<html><head></head><body><div id='link'><span id='next'></span><br><span id='prev'></span></div><div id='messages'></div></body></html>";
+var messageTemplate = "<span><b id='nickname'></b>: <span id='msg'></span></span><br>";
+var talkLinkTemplate = "<a href='href' id='link'></a>";
